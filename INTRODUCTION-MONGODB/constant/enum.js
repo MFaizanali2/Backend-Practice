bf5a08cd7b6f31f41f63535045bb50a32c809340
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2024-12-29 14:24:36 +0500

    fest

---
+++ INTRODUCTION-MONGODB/constant/enum.js
@@ -0,0 +1,7 @@
+export const enums = {
+    ERROR_CONNECTION : "something went wrong",
+    SUCCES: "Success",
+    SUCCES_MSG : "Added succesfully",
+    DELETE_MSG : "Delete succesfully",
+    UPDATE_MSG : "Update succesfully"
+}
